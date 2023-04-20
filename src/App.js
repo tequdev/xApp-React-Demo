cd87fc73e29077d9b9e06112ce3077a666dce996
tequ <git@tequ.dev>
2023-04-20 19:35:17 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Xumm } from "xumm";
+
+const xumm = new Xumm("api-key");
 
 function App() {
+  const [account, setAccount] = useState(null);
+  xumm.user.account.then((a) => setAccount(a));
+  const createTransaction = async () => {
+    const payload = await xumm.payload?.create({
+      TransactionType: "Payment",
+      Destination: "rQQQrUdN1cLdNmxH4dHfKgmX5P4kf3ZrM",
+      Amount: "1000000",
+    });
+    xumm.xapp?.openSignRequest({
+      uuid: payload?.uuid,
+    });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Address: {account}
+        <button className="Payment-button" onClick={createTransaction}>
+          Create Payment Transaction
+        </button>
+        <button className="Close-button" onClick={() => xumm.xapp?.close()}>
+          Close xApp
+        </button>
       </header>
     </div>
   );
